5dbe69853c4b25110dfc6c01ea84c8090a62d8c7
trungquan21 <89087554+trungquan21@users.noreply.github.com>
2024-02-22 10:54:10 +0700

    #fix

--- api-opendata/Program.cs
+++ api-opendata/Program.cs
@@ -117,7 +117,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "WRQuangNgai API");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "OpenData API");
     });
 }
 
